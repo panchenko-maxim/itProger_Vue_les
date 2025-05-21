dc44aac8702ceb225cc06b24a42eb6b6472650ac
root221 <panchenko.maxim87@gmail.com>
2025-05-21 16:09:58 +0300

    end lesson 3

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <h3>{{ info }}</h3>
+  <p>{{ someInfo }}</p>
+
+  <button type="button" @click="userData('Some new')">Send</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      info: 'Title',
+      someInfo: 'Anons of message',
+    }
+  },
+  methods: {
+    userData(word = 'New text') {
+      // this.info = 'Some new'
+      this.someInfo = word
+    }
+  }
+}
+</script>
+
+<style scoped>
+  h3 {
+    font-weight: lighter;
+  }
+
+  p {
+    color: rgb(203, 31, 31);
+  }
+</style>
